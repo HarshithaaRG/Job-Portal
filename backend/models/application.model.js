84828096ea6adbdf615478b88a3c845030d4242b
HarshithaaRG <harshithaa_rg@yahoo.com>
2024-12-12 23:15:06 +0530

    models

--- backend/models/application.model.js
+++ backend/models/application.model.js
@@ -4,5 +4,16 @@ const application=new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:'Job',
         required:true
+    },
+    applicant:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    status:{
+        type:String,
+        enum:['pending','accepted','rejected'],
+        default:'pending'
     }
-})
+},{timeseries:true});
+export const Application=mongoose.model("Application",application);
