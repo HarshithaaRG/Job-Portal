cd6990147328a349d7525fe8c6e52ab47cd501d9
HarshithaaRG <harshithaa_rg@yahoo.com>
2024-12-14 11:40:41 +0530

    middlewares

--- backend/controllers/user.contoller.js
+++ backend/controllers/user.contoller.js
@@ -25,10 +25,13 @@ export const register=async (req , res)=>{
             password:hashedPassword,
             role
         })
+        return res.status(201).json({
+            message:"Account created successfully",
+            success:true
+        })
     }
     catch(error){
-
-
+        console.log(error);
     }
 }
 export const login=async(req,res)=>{
@@ -40,7 +43,7 @@ export const login=async(req,res)=>{
                 success:false
             })
         }
-        const user=User.findOne({email});
+        let user=User.findOne({email});
         if(!user){
             return res.status(400).json({
                 message:"Incorrect email or password",
@@ -65,6 +68,15 @@ export const login=async(req,res)=>{
             userId:user_.id
         }
         const token=await jwt.sign(tokenData,process.env.SECRET_KEY,{expiresIn:'1d'});
+
+        user={
+            _id:user._id,
+            fullname:user.fullname,
+            email:user.email,
+            phoneNumber:user.phoneNumber,
+            role:user.role,
+            profile:user.profile
+        }
         return res.status(200).cookie("token",token,{maxAge:1*24*60*60*1000,httpsOnly:true,sameSite:'strict'}).json({
             message:`welcome back ${user.fullname}`,
             success:true,
@@ -72,6 +84,66 @@ export const login=async(req,res)=>{
 
     }
     catch(error){
+        console.log(error);
 
+    }
+}
+export const logout=async(req,res)=>{
+    try{
+        return res.status(200).cookie("token","", {maxAge:0}).json({
+            message:"Logout successfull",
+            success:true
+        })
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+export const updateProfile=async(req,res)=>{
+    try{
+        const {fullname,email,phoneNumber,bio,skills}=req.body;
+        if(!fullname || !email || !phoneNumber || !bio || !skills){
+            return res.status(400).json({
+                message:'Some details are missing',
+                success:false
+            })
+        }
+
+        //cloudinary
+
+        const skillsArray=skills.split(",");
+        const userId=req.id;//middleware auth
+        let user=await User.findById(userId);
+        if(user){
+            return res.status(400).json({
+                message:"user not found",
+                success:false
+            })
+        }
+        //updating data
+        user.fullname=fullname,
+        user.email=email,
+        user.phoneNumber=phoneNumber,
+        user.profile.bio=bio,
+        user.profile.skills=skillsArray
+        //resume come later 
+
+        await user.save()
+
+        user={
+            _id:user._id,
+            fullname:user.fullname,
+            email:user.email,
+            phoneNumber:user.phoneNumber,
+            role:user.role,
+            profile:user.profile
+        }
+        return res.status(200).json({
+            message:'Profile updated successfully',
+            user,
+            success:true,
+        })
+    }catch(error){
+        console.log(error)
     }
 }
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js"
 dotenv.config({});
 
 const app=express();
@@ -19,6 +20,9 @@ app.use(cors(corsOptions));
 
 
 const PORT=process.env.PORT || 3000;
+
+app.use("/api/v1/user",userRoute);
+
 app.listen(PORT,()=>{
     connectDB();
     console.log(`Server running in port ${PORT}`)
